4a6ff9ab65f3cd348681c5840b73b61702346c4f
Marcio Souza <marciioasouzza@gmail.com>
2026-01-22 22:25:29 -0300

    feat: initial commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/")({
+  component: Index,
+});
+
+function Index() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  );
+}
