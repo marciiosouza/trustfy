294da45ba28bcc57764349a8c5de8eab1397e7cd
Marcio Souza <marciioasouzza@gmail.com>
2026-01-24 15:37:05 -0300

    feat: create hero page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { HeroPage } from "@/pages/hero";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
@@ -6,7 +7,6 @@ export const Route = createFileRoute("/")({
 
 function Index() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-    </div>
+    <HeroPage />
   );
 }
